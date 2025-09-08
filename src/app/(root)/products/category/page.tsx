9de4fd9acedd4648deaf324a2298fd48c06c41ab
dylan/ <dilanefotio14@gmail.com>
2025-09-08 18:01:05 +0100

    new gig

--- src/app/(root)/products/category/page.tsx
+++ src/app/(root)/products/category/page.tsx
@@ -70,7 +70,7 @@ const Category = () => {
   }, [category.allIds, category.byId, filter]);
 
   return (
-    <div className="p-6 bg-white dark:bg-gray-800 rounded-lg shadow-md">
+    <div className=" bg-white dark:bg-gray-800 rounded-lg shadow-md">
       <h3 className="text-xl font-semibold mb-4 text-gray-800 dark:text-white">
         Select Category
       </h3>
