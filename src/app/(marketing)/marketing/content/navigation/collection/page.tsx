b859a44f7d6e929251148bce5a7a41ecc57ec6ed
fotio dylan <dilanefotio4@gmail.com>
2026-08-26 13:35:29 +0100

    new update

--- src/app/(marketing)/marketing/content/navigation/collection/page.tsx
+++ src/app/(marketing)/marketing/content/navigation/collection/page.tsx
@@ -169,7 +169,7 @@ const ProductCollectionPage = () => {
             <p className="mt-1 text-gray-500">
               Get started by creating a new collection.
             </p>
-            <div className="mt-6">
+            <div className="mt-6  bg-primary">
               <Link
                 href="/marketing/content/navigation/collection/create"
                 className="inline-flex items-center px-4 py-2  border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-primary hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
